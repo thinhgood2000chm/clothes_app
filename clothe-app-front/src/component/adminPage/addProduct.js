563223fe39507e6ddee0d59c684bfe943ff25574
thinhnx <thinhnx@seatechit.com.vn>
2024-04-16 15:58:26 +0700

    add product

--- clothe-app-front/src/component/adminPage/addProduct.js
+++ clothe-app-front/src/component/adminPage/addProduct.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import '../../static/css/admin.css'
+import { createProduct } from '../../common/api/product';
 function AddProductForm() {
     const [productName, setProductName] = useState('');
-    const [productColor, setProductColor] = useState('');
+    const [productColor, setProductColor] = useState([]);
     const [productType, setProductType] = useState('');
     const [productPrice, setProductPrice] = useState('');
     const [productQuantity, setProductQuantity] = useState('');
     const [productDescription, setProductDescription] = useState('');
     const [mainImage, setMainImage] = useState('');
+    const [mainImageUpload, setMainImageUpload] = useState()
     const [galleryImages, setGalleryImages] = useState([]);
+    const [subImageUpload, setSubImagesUpload] = useState([]);
 
+
+    const addNewProduct = async (productData) =>{
+        await createProduct(productData)
+    }
     const handleProductImageChange = (event) => {
         const file = event.target.files[0];
+        setMainImageUpload(file)
         const reader = new FileReader();
         reader.onload = () => {
             setMainImage(reader.result);
@@ -21,6 +29,7 @@ function AddProductForm() {
 
     const handleGalleryImagesChange = (event) => {
         const files = event.target.files;
+        setSubImagesUpload( Array.from(files))
         const images = [];
         for (let i = 0; i < files.length; i++) {
             const file = files[i];
@@ -35,6 +44,69 @@ function AddProductForm() {
         }
     };
 
+
+    function setValueProductName(event){
+        var prodName = event.target.value
+        setProductName(prodName)
+    }
+    function setValueProductDescription(event){
+        var prodDes = event.target.value
+        setProductDescription(prodDes)
+    }
+    function setValueProductPrice(event){
+        var prodprice = event.target.value
+        setProductPrice(prodprice)
+    }
+    function setValueProductQuantity(event){
+        var prodprice = event.target.value
+        setProductQuantity(prodprice)
+    }
+   
+    function setValueProductCategory(event){
+        var prodCate = event.target.value
+        console.log(prodCate)
+        setProductType(prodCate)
+    }
+
+
+    function setColors(event){
+        var colors = productColor
+        var prodColor = event.target.getAttribute('data-color')
+        var colorId = event.target.getAttribute('id')
+        var chosenColor = document.getElementById(colorId)
+        if(chosenColor.style.border){
+            chosenColor.style.border = null
+            var index = colors.indexOf(prodColor);
+            colors.splice(index, 1);
+        }else{
+            colors.push(prodColor)
+            setProductColor(colors)
+            chosenColor.style.border = "2px solid black"
+          
+        }
+    }
+    function addNewProd(){
+        var formData = new FormData();
+        formData.append('list_color_code', productColor);
+        formData.append('product_name', productName);
+        formData.append('quantity', productQuantity);
+        formData.append('price', productPrice);
+        formData.append('category', productType);
+        formData.append('description', productDescription);
+        formData.append('main_image_upload', mainImageUpload);
+
+        if(subImageUpload?.length > 0 ){
+            subImageUpload.forEach((image, index) => {
+                formData.append(`list_image_upload`, image);
+              });
+        }
+
+        // formData.append('video_upload', postVideo);
+        // setOnLoading(true)
+        // callApiCreateNewPost(formData)
+
+        addNewProduct(formData)
+    }
     return (
         <div class="add-content-product-container">
         <div className="container content-form-product">
@@ -42,22 +114,22 @@ function AddProductForm() {
             <form >
             <div class="form-group">
                 <label for="productName">Product Name:</label>
-                <input type="text" id="productName" name="productName" required/>
+                <input type="text" id="productName" name="productName" onChange={setValueProductName} required/>
             </div>
             <div class="form-group">
                 <label for="productColor">Product Color:</label>
                 <div class="color-palette">
-                    <div class="color" style={{backgroundColor: "#ff0000"}}></div>
-                    <div class="color" style={{backgroundColor: "#00ff00"}}></div>
-                    <div class="color" style={{backgroundColor: "#0000ff"}}></div>
-                    <div class="color" style={{backgroundColorr: "#ffff00"}}></div>
-                    <div class="color" style={{backgroundColor: "#ff00ff"}}></div>
-                    <div class="color" style={{backgroundColor: "#00ffff"}}></div>
+                    <div class="color" id = "c1"style={{backgroundColor: "#ff0000"}} data-color="#ff0000" onMouseDown={setColors}></div>
+                    <div class="color " id = "c2" style={{backgroundColor: "#00ff00"}} data-color="#00ff00" onMouseDown={setColors}></div>
+                    <div class="color " id = "c3" style={{backgroundColor: "#0000ff"}} data-color="#0000ff"  onMouseDown={setColors}></div>
+                    <div class="color " id = "c4" style={{backgroundColorr: "#ffff00"}} data-color="#ffff00"  onMouseDown={setColors}></div>
+                    <div class="color " id = "c5" style={{backgroundColor: "#ff00ff"}} data-color="#ff00ff"  onMouseDown={setColors}></div>
+                    <div class="color " id = "c6" style={{backgroundColor: "#00ffff"}} data-color="#00ffff"  onMouseDown={setColors}></div>
                 </div>
             </div>
             <div class="form-group">
                 <label for="productType">Product Type:</label>
-                <select id="productType" name="productType" required>
+                <select id="productType" name="productType" value={productType} onChange={setValueProductCategory} required>
                     <option value="type1">Type 1</option>
                     <option value="type2">Type 2</option>
                     <option value="type3">Type 3</option>
@@ -68,17 +140,17 @@ function AddProductForm() {
                 <div class="input-group">
                     <div class="input-field">
                         <label for="productPrice">Product Price:</label>
-                        <input type="number" id="productPrice" name="productPrice" required/>
+                        <input type="number" id="productPrice" name="productPrice"  onChange={setValueProductPrice} required/>
                     </div>
                     <div class="input-field">
                         <label for="productQuantity">Product Quantity:</label>
-                        <input type="number" id="productQuantity" name="productQuantity" required/>
+                        <input type="number" id="productQuantity" name="productQuantity"  onChange={setValueProductQuantity} required/>
                     </div>
                 </div>
             </div>
             <div class="form-group">
                 <label for="productDescription">Product Description:</label>
-                <textarea id="productDescription" name="productDescription" rows="4" required></textarea>
+                <textarea id="productDescription" name="productDescription" onChange={setValueProductDescription} rows="4" required></textarea>
             </div>
                 {/* Add other form fields similarly */}
                 <div className="form-group">
@@ -93,7 +165,7 @@ function AddProductForm() {
                         <img key={index} src={image} alt={`Gallery Preview ${index + 1}`} style={{ maxWidth: '200px', marginTop: '10px', marginRight: '10px' }} />
                     ))}
                 </div>
-                <button type="button">Add Product</button>
+                <button type="button" onClick={addNewProd}>Add Product</button>
             </form>
         </div>
         </div>
